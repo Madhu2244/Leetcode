231257686134c5f73d8e9ef7592e7e364687abc5
Madhu2244 <63474175+Madhu2244@users.noreply.github.com>
2021-08-25 22:16:46 -0500

    Update 21. Merge Two Sorted Lists.py

--- 21. Merge Two Sorted Lists.py
+++ 21. Merge Two Sorted Lists.py
@@ -20,4 +20,69 @@ loop until both a and b are len(list) respecitvely
 		append apointer into ListNode
 		apointer++
 		continue
+result: 1 --> ( )
+               ^result.next
+l1: 2 --> 3 --> 4
+
+
+traverser = result
+traverser.next = (node) l2
+traverser = traverser.next
+
+result: 1 --> 1 --> ( )
+
+l1 = 3 --> 4
+
 """
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        #checking if either list is empty
+        if l1 == None:
+            return l2
+        elif l2 == None:
+            return l1
+    
+    
+        #creating initial node
+        result = ListNode(-1)
+        if l1.val > l2.val:
+            result.val = l2.val
+            l2 = l2.next
+        elif l1.val <= l2.val:
+            result.val = l1.val
+            l1 = l1.next
+        
+        #creating traverser to go through result node
+        traverser = result
+        while l1 != None or l2 != None:
+            if l1 == None:
+                traverser.next = ListNode(l2.val)
+                l2 = l2.next
+                traverser = traverser.next
+            elif l2 == None:
+                traverser.next = ListNode(l1.val)
+                l1 = l1.next
+                traverser = traverser.next
+            elif l1.val > l2.val:
+                traverser.next = ListNode(l2.val)
+                l2 = l2.next
+                traverser = traverser.next
+            elif l1.val <= l2.val:
+                traverser.next = ListNode(l1.val)
+                l1 = l1.next
+                traverser = traverser.next
+        return result
+            
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        
