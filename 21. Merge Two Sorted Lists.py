478c3d29e28cc55f3a6af70ad369cbc856839d87
Madhu2244 <63474175+Madhu2244@users.noreply.github.com>
2021-08-25 19:32:45 -0500

    Create 21. Merge Two Sorted Lists.py

---
+++ 21. Merge Two Sorted Lists.py
@@ -0,0 +1,23 @@
+"""
+1 2 4   1 3 4
+a       b
+
+create an empty ListNode
+loop until both a and b are len(list) respecitvely
+	if bpointer > len(list)
+		append apointer into ListNode
+		apointer++
+		continue
+	else if apointer > len(list)
+		append apointer into ListNode
+		apointer++
+		continue
+	if value at apointer is greater than value at bpointer,
+		append bpointer into ListNode
+		bpointer++
+		continue
+	else
+		append apointer into ListNode
+		apointer++
+		continue
+"""
