0de159b2470a083296826e01d41966c50fe7a566
Madhu2244 <63474175+Madhu2244@users.noreply.github.com>
2021-08-26 22:15:04 -0500

    Update 28. Implement strStr().java

--- 28. Implement strStr().java
+++ 28. Implement strStr().java
@@ -3,3 +3,16 @@ class Solution {
         return haystack.indexOf(needle);
     }
 }
+/*
+my python solution too
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        return haystack.find(needle)
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        
+*/
