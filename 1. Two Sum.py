708649fbd89b18f05afbd0cd7c37c06f1d61e4f2
Madhu2244 <63474175+Madhu2244@users.noreply.github.com>
2021-08-28 19:32:40 -0500

    Create 1. Two Sum.py

---
+++ 1. Two Sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        #initial pass to get values into dictionary
+        for i,n in enumerate(nums):
+            if n in d:
+                d[n][0] += 1
+                d[n][1].append(i)
+            else:
+                d[n] = [1,[i]]
+        #print(d)
+        
+        
+        #second pass to go through each element and check if the opposite is in the dictionary to add up to target
+        for i,n in enumerate(nums):
+            leftover = target - n
+            #check if leftover value is in dictionary
+            if leftover in d:
+                #check if leftover value is the same as current index
+                if leftover == n:
+                    if d[leftover][0] > 1:
+                        result = [i,d[leftover][1][1]]
+                        break
+                else:
+                    result = [i,d[leftover][1][0]]
+                    break
+                          
+        return result
