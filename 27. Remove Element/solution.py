3e1765dce796a84f2f8d9bdcec95b7ea12f8fd69
Madhu2244 <63474175+Madhu2244@users.noreply.github.com>
2021-08-31 01:41:27 -0500

    Updated Faster Solution

--- 27. Remove Element/solution.py
+++ 27. Remove Element/solution.py
@@ -1,6 +1,9 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        for i,n in reversed(list(enumerate(nums))):
-            if n == val:
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
                 nums.pop(i)
+                i -= 1
+            i += 1
         return len(nums)
