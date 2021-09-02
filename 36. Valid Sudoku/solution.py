d3297a85c27da11d87e5e5b1282ba9a7f6168b0f
Madhu2244 <63474175+Madhu2244@users.noreply.github.com>
2021-09-01 21:02:17 -0500

    Update solution.py

--- 36. Valid Sudoku/solution.py
+++ 36. Valid Sudoku/solution.py
@@ -1,35 +1,28 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        #1.
-        for row in board:
-            seen_numbers = set()
-            for num in row:
-                if num.isnumeric():
-                    if num in seen_numbers:
-                        print('step 1 fail')
-                        return False
-                    else:
-                        seen_numbers.add(num)
-        #2.
-        for column in range(9):
-            seen_numbers = set()
-            for index in range(9):
-                num = board[index][column]
-                print(num)
-                if num.isnumeric():
-                    if num in seen_numbers:
-                        print('step 2 fail')
-                        return False
-                    else:
-                        seen_numbers.add(num)
-        #3.
+        seen_row_numbers = [set() for i in range(9)]
+        seen_column_numbers = [set() for i in range(9)]
         for sector in range (3):
             for quadrant in range (3):
                 seen_numbers = set()
                 for row in range (3):
                     for index in range (3):
                         num = board[row+sector*3][index+quadrant*3]
+                        cur_row = sector * 3 + row
+                        column = index + quadrant * 3                        
                         if num.isnumeric():
+                            if num in seen_row_numbers[cur_row]:
+                                print('step 1 fail')
+                                return False
+                            else:
+                                seen_row_numbers[cur_row].add(num)
+
+                            if num in seen_column_numbers[column]:
+                                print('step 2 fail')
+                                return False
+                            else:
+                                seen_column_numbers[column].add(num)
+                            
                             if num in seen_numbers:
                                 print('step 3 fail')
                                 return False
